53a0ae5f7676a53a67fa766bc9eadcad8357c89e
IchhaOberoi <ichhaoberoi08@gmail.com>
2025-04-23 18:42:58 +0530

    model trainer and model evaluation added

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -20,4 +20,31 @@ class DataTransformationConfig:
     root_dir: Path
     data_path: Path
     tokenizer_name: Path
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    data_path: Path
+    model_ckpt: Path
+    num_train_epochs: int
+    per_device_train_batch_size: int
+    per_device_eval_batch_size: int
+    logging_steps: int
+    save_strategy: str
+    remove_unused_columns: bool
+    report_to: str
+    gradient_accumulation_steps: int
+    dataloader_num_workers: int
+    fp16: bool
+    seed: int
+    load_best_model_at_end: bool
+    metric_for_best_model: str
     
+    
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
